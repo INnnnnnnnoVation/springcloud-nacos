c27cc4c10c0f601dfe774527b1d367782432135a
winge <1771438509@qq.com>
2023-02-14 14:41:58 +0800

    update

---
+++ src/main/java/com/example/springcloudnacos/SpringcloudNacosApplication.java
@@ -0,0 +1,15 @@
+package com.example.springcloudnacos;
+
+import org.apache.commons.logging.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudNacosApplication.class, args);
+        System.out.println("start=============");
+    }
+
+}
